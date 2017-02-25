c2af3c214590cf2b23531fb44c246414dc93ded4
slowikps <slowikps@gmail.com>
2017-02-25 20:58:18 +0000

    Either - finall impl

--- app-2.11/learn/fpscala/exercise/chapter4/Either.scala
+++ app-2.11/learn/fpscala/exercise/chapter4/Either.scala
@@ -3,21 +3,42 @@ package learn.fpscala.exercise.chapter4
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(r) => Right(f(r))
+    case l@ Left(_) => l
+  }
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case l@ Left(_) => l
+    case Right(x) => f(x)
+  }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case r@ Right(_) => r
+  }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      a <- this
+      b <- b
+    }yield (f(a,b))
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es match  {
+      case Nil => Right(Nil)
+//      case x :: xs => traverse(xs)(f).flatMap(r => f(x).map(xx => List(xx) ::: r))
+      case h::t => (f(h) map2 traverse(t)(f))(_ :: _)
+    }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def traverse_1[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight[Either[E,List[B]]](Right(Nil))((a, b) => f(a).map2(b)(_ :: _))
+
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = traverse(es)(a => a)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] =
     if (xs.isEmpty)
--- app-2.11/learn/fpscala/exercise/chapter4/Main.scala
+++ app-2.11/learn/fpscala/exercise/chapter4/Main.scala
@@ -1,25 +1,45 @@
 package learn.fpscala.exercise.chapter4
-import scala.{Option => _, Some => _, Either => _, _}
+import scala.{Option => _, Some => _, Either => _, Right => _, Left => _}
 
 object Main {
 
+
   def main(args: Array[String]): Unit = {
+    eitherTest()
+    //optionTest
+  }
+
+  def eitherTest() = {
+    println{
+      "Right map: " + Right(11).map("value: " + _)
+    }
+    println{
+      "Right map: " + (Left("Big bad error"): Either[String, Int]).map("value: " + _)
+    }
+
+    println{
+      "Traverse: " + (Either.traverse(List(1,2,3))(a => Right(s"the value is: $a")))
+    }
+
+  }
+
+  private def optionTest() = {
     println(
       Some("Pawel").orElse(Some("Gawel"))
     )
 
     println(
       "No None in the sequence: " + Option.sequence(List(Some(1), Some(2), Some(3))) + ", " +
-       Option.sequence4(List(Some(1), Some(2), Some(3)))
+        Option.sequence4(List(Some(1), Some(2), Some(3)))
     )
 
     println(
       "None in the sequence: " + Option.sequence(List(Some(1), Some(2), Some(3), Some(4), None, Some(5))) + ", " +
-       Option.sequence4(List(None, Some(1), Some(2), Some(3), Some(4), None, Some(5)))
+        Option.sequence4(List(None, Some(1), Some(2), Some(3), Some(4), None, Some(5)))
     )
 
-    println{
-      "Traverse result: " + Option.traverse(List(1,2,3,4))(Some(_))
+    println {
+      "Traverse result: " + Option.traverse(List(1, 2, 3, 4))(Some(_))
     }
   }
 }
